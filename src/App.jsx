97b2e804144b8fc50ede37ed7a228316bc2876ed
shamsiddin <devshamsiddin@gmain.com>
2024-10-31 17:55:38 +0500

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,22 @@ import InfoCarousel from "./components/Info";
 import Cost from "./components/Cost";
 import RegistrationPage from "./components/Register";
 import GalleryPage from "./components/Galary";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 // import Team from "./components/Team";
-import qiz from './assets/10.png'
-import bola from './assets/1.png'
-import { useState } from "react";
-import { FaSun } from "react-icons/fa";
 
 
 const App = () => {
-  const [sun,setSun] = useState(["1",'2','3'])
   return (
     <div>
-     {/* <Navbar/>
+     <Navbar/>
      <Hero/>
      <About/>
      <CustomCarousel/>
      <InfoCarousel/>
      <Cost/>
      <RegistrationPage/>
-     <GalleryPage/> */}
-     {/* <Footer/> */}
-     {/* {ism?<img src={bola} alt="" />:<img src={qiz}/>}
-     <button onClick={()=>setIsm(!ism)} className="bg-slate-400 p-5">{ism?"Bola":"Qiz"}</button> */}
-    
-     {/* {sun && <img onClick={()=>setSun(!sun)} className="w-20 active:rotate-180 transition-all" src="/src/assets/moon.png"/>} 
-     {!sun && <img onClick={()=>setSun(!sun)} className="w-20 active:rotate-180 transition-all" src="/src/assets/sun.png"/>}  */}
-      {["https://picsum.photos/200/400",'https://picsum.photos/200/300','https://picsum.photos/400/300'].map((v,i)=><div  className="h-20 bg-slate-300 w-20 mb-5">
-        <img src={v} alt="" />
-      </div>) }
-      
+     <GalleryPage/>
+     <Footer/>
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,34 @@
-// import React from "react";
+import React from "react";
 
-// const Footer = () => {
-//   return (
-//     <div className="bg-gray-800 text-white p-4">
-//       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-//         <div className="mb-4 md:mb-0">
-//           <h5 className="text-xl font-bold">Company Name</h5>
-//           <p>Address, City</p>
-//           <p>More info here</p>
-//         </div>
-//         <div className="w-full md:w-1/2 lg:w-1/3">
-//           <div className="h-[460px] mt-10 mb-16 shadow-lg">
-//             <iframe
-//               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13044.305524121615!2d69.22257488393805!3d41.31916884309184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8bcd3b798b93%3A0x8012ce8c311c2666!2sHanyuban%20o%CA%BBquv%20markazi!5e0!3m2!1sen!2s!4v1724894763853!5m2!1sen!2s"
-//               height="460"
-//               style="border:0;"
-//               allowfullscreen=""
-//               className="w-full"
-//               loading="lazy"></iframe>
-//           </div>
-//           <img
-//             src="map-placeholder.png"
-//             alt="Map"
-//             className="max-w-full h-auto"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="mb-4 md:mb-0">
+          <h5 className="text-xl font-bold">Company Name</h5>
+          <p>Address, City</p>
+          <p>More info here</p>
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/3">
+          <div className="h-[460px] mt-10 mb-16 shadow-lg">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13044.305524121615!2d69.22257488393805!3d41.31916884309184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8bcd3b798b93%3A0x8012ce8c311c2666!2sHanyuban%20o%CA%BBquv%20markazi!5e0!3m2!1sen!2s!4v1724894763853!5m2!1sen!2s"
+              height="460"
+              style="border:0;"
+              allowfullscreen=""
+              className="w-full"
+              loading="lazy"></iframe>
+          </div>
+          <img
+            src="map-placeholder.png"
+            alt="Map"
+            className="max-w-full h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 
-// export default Footer;
+export default Footer;
